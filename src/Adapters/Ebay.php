37b922fafb073c33f78d3f1a559f4ba54bc17737
Claudio Giordano <claudio.giordano@autistici.org>
2017-04-04 21:36:50 +0200

    Add getAppToken management with handle for invalid / expired tokens

--- src/Adapters/Ebay.php
+++ src/Adapters/Ebay.php
@@ -12,14 +12,50 @@ use \DTS\eBaySDK\OAuth\Types;
  */
 class Ebay extends AbstractAdapter
 {
-    protected $service;
+    protected $service = null;
+    protected $appToken = null;
+    protected $appTokenExpireAt = null;
 
-    public function __construct(Config $config)
+    public function __construct(Config $config, $sandboxMode = true)
     {
+        $section = 'sandbox';
+        if (!$sandboxMode) {
+            $section = 'production';
+        }
+
         $this->service = new Services\OAuthService([
-            'credentials' => $config['sandbox']['credentials'],
-            'ruName'      => $config['sandbox']['ruName'],
-            'sandbox'     => true
+            'credentials' => $config->getValue("{$section}.credentials"),
+            'ruName'      => $config->getValue("{$section}.ruName"),
+            'sandbox'     => $sandboxMode
         ]);
+    }
+
+    /**
+     * Store and returns access token data information, cache and validate token validity,
+     * require a new token if invalid or expired
+     *
+     * @return string
+     */
+    public function getAppToken()
+    {
+        /**
+         * Validate if token exists and if is expired
+         */
+        if (!is_null($this->appToken) && ($this->appTokenExpireAt > time())) {
+            return $this->appToken;
+        }
+
+        /**
+         * Get token data
+         */
+        $response = $this->service->getAppToken();
+
+        /**
+         * Store access_token and expire time
+         */
+        $this->appToken = $response->access_token;
+        $this->appTokenExpireAt = strtotime("+ {$response->expires_in} seconds");
+
+        return $this->appToken;
     }
 }
--- tests/test.php
+++ tests/test.php
@@ -4,7 +4,11 @@ namespace clagiordano\MarketplacesDataExport\Tests;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use clagiordano\MarketplacesDataExport\Adapters\Ebay;
 use clagiordano\MarketplacesDataExport\Config;
 
 $config = new Config(__DIR__ . '/../testdata/ebay.php');
-var_dump($config->getValue('sandbox'));
+
+$ebay = new Ebay($config);
+print_r($ebay->getAppToken());
+
