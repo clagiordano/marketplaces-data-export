f0349c53427282d98c3e501bc4ed9289f0489b23
Claudio Giordano <claudio.giordano@autistici.org>
2017-04-11 00:11:26 +0200

    Add shipping object

---
+++ src/Shipping.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace clagiordano\MarketplacesDataExport;
+
+/**
+ * Class Customer
+ * @package clagiordano\MarketplacesDataExport
+ */
+class Shipping
+{
+    /** @var string|null $shippingAddress */
+    public $shippingAddress = null;
+    /** @var string|null $shippingContact */
+    public $shippingContact = null;
+    /** @var int|null $postalCode */
+    public $postalCode = null;
+}
