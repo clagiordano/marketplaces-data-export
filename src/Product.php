2f46dd08cc332aec68d3c77f341d5a1f6a6592ed
Claudio Giordano <claudio.giordano@autistici.org>
2017-06-13 19:45:03 +0200

    Add magic method for extra product properties

--- src/Product.php
+++ src/Product.php
@@ -14,4 +14,27 @@ class Product
     public $vendorProductId = null;
     /** @var string|null $description */
     public $description = null;
+    /** @var int $storedAmount */
+    public $storedAmount = 0;
+
+    /**
+     * @param string $name
+     */
+    function __get($name)
+    {
+        if (isset($this->{$name})) {
+            return $this->{$name};
+        }
+    }
+
+    /**
+     * @param string $name
+     * @param mixed $value
+     */
+    function __set($name, $value)
+    {
+        $this->{$name} = $value;
+    }
+
+
 }
