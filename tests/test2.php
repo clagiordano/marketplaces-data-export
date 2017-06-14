44fba0eb4a34694b4c3e70a3c78ab8ff00945bf6
Claudio Giordano <claudio.giordano@autistici.org>
2017-06-14 13:49:06 +0200

    test export ebay storage

--- tests/test2.php
+++ tests/test2.php
@@ -6,10 +6,26 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use clagiordano\MarketplacesDataExport\Adapters\Ebay;
 use clagiordano\MarketplacesDataExport\Config;
-use clagiordano\MarketplacesDataExport\Transaction;
+use clagiordano\MarketplacesDataExport\Product;
 
 $config = new Config(__DIR__ . '/../testdata/ebay.php');
 
 $ebay = new Ebay($config, false);
 
-$ebay->getSellingList();
+$list = $ebay->getSellingList();
+
+$outFile = __DIR__ . '/../testdata/out/out_sample.csv';
+$outLine = "Codice Prodotto;Descrizione;Quantità disponibile;Quantità\n";
+file_put_contents($outFile, $outLine);
+
+/** @var Product $prod */
+foreach ($list as $prod) {
+    $outLine = $prod->vendorProductId . ";";
+    $outLine .= $prod->description . ";";
+    $outLine .= $prod->availableAmount . ";";
+    $outLine .= $prod->storedAmount . "\n";
+
+    file_put_contents($outFile, $outLine, FILE_APPEND);
+}
+
+echo "Done!\n";
