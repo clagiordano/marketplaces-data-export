fe17f3a2e8b203317f848eee1d26db4c84be96ea
Claudio Giordano <claudio.giordano@autistici.org>
2018-01-31 19:53:03 +0100

    Add getAppToken works and update constructor for mws

--- src/Adapters/AmazonMws.php
+++ src/Adapters/AmazonMws.php
@@ -30,9 +30,13 @@ class AmazonMws extends AbstractAdapter
             $section = 'production';
         }
 
-        $this->serviceConfig = [];
-        $this->appToken = $this->adapterConfig->getValue("{$section}.authToken");
-        $this->appTokenExpireAt = strtotime("+ 7200 seconds");
+        $this->serviceConfig = [
+            'Marketplace_Id' => $this->adapterConfig->getValue("{$section}.Marketplace_Id"),
+            'Seller_Id' => $this->adapterConfig->getValue("{$section}.Seller_Id"),
+            'Access_Key_ID' => $this->adapterConfig->getValue("{$section}.Access_Key_ID"),
+            'Secret_Access_Key' => $this->adapterConfig->getValue("{$section}.Secret_Access_Key"),
+            'MWSAuthToken' => $this->adapterConfig->getValue("{$section}.MWSAuthToken"),
+        ];
     }
 
     /**
@@ -40,7 +44,17 @@ class AmazonMws extends AbstractAdapter
      */
     public function getAppToken()
     {
-        // TODO: Implement getAppToken() method.
+        /**
+         * Validate if token is set
+         */
+        if (!is_null($this->appToken)) {
+            return $this->appToken;
+        }
+
+        $this->appToken = $this->serviceConfig['MWSAuthToken'];
+        $this->appTokenExpireAt = strtotime("+ 7200 seconds");
+
+        return $this->appToken;
     }
 
     /**
